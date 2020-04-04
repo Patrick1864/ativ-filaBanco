6796b8a9cc00aa5623e655c6df7197d71a529003
Pedro <61153395+DPedroPS@users.noreply.github.com>
2020-04-04 17:25:17 -0300

    Update ManageAttendance.java
    Implementado atributos e metodos

--- src/ManageAttendance.java
+++ src/ManageAttendance.java
@@ -2,32 +2,49 @@ import java.util.ArrayList;
 
 public class ManageAttendance {
 	
-	private ArrayList<Client> queue = new ArrayList<Client>();
+	private ArrayList<Client> communQueue = new ArrayList<Client>();
+	private ArrayList<Client> priorityQueue = new ArrayList<Client>();
+	private int ordenador = 0;
 	
-	
-
 	public ManageAttendance(int size) {
 		super();	
 	}
 	
 	public boolean isEmpty() {
-		return true;
+		return (this.communQueue.isEmpty() && this.priorityQueue.isEmpty() );
 	}
 	
 	public int numClients() {
-		return 0;
+		return this.communQueue.size() + this.priorityQueue.size();
 	}
 	
 	public int numElderlyClients() {
-		return 0;
+		return this.priorityQueue.size();
 	}
 	
+	
 	public void addClient(Client client) {
-		
+		if(client.getAge() >= 60) {
+			this.priorityQueue.add(client);
+		}else {
+			this.communQueue.add(client);
+		}
 	}
 	
 	public Client showNext() {
-		return null;
+		Client temp;
+		if(!this.communQueue.isEmpty() && !this.priorityQueue.isEmpty()) {
+			if(this.ordenador <= 2) {
+				temp = this.priorityQueue.get(0);
+			}else {
+				temp = this.communQueue.get(0);
+			}
+		} else if(!this.communQueue.isEmpty() && this.priorityQueue.isEmpty()) {
+			temp = this.communQueue.get(0);
+		} else {
+			temp = this.priorityQueue.get(0);
+		}
+		return temp;
 	}
 	
 	public Client getNext() {
