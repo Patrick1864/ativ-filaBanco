1f28f93602530b736b6f95c4f6d832041d7ce837
Pedro <>
2020-04-04 17:39:34 -0300

    Update ManageAttendance.java
    Implementado os metodos

--- src/ManageAttendance.java
+++ src/ManageAttendance.java
@@ -47,22 +47,57 @@ public class ManageAttendance {
 		return temp;
 	}
 	
-	public Client getNext() {
-		return null;
+	ppublic Client getNext() {
+		Client temp;
+		if(!this.communQueue.isEmpty() && !this.priorityQueue.isEmpty()) {
+			if(this.ordenador <= 2) {
+				this.ordenador++;
+				this.priorityQueue.remove(0);
+				temp = this.priorityQueue.get(0);
+			}else {
+				this.ordenador = 0;
+				this.communQueue.remove(0);
+				temp = this.communQueue.get(0);
+			}
+		} else if(!this.communQueue.isEmpty() && this.priorityQueue.isEmpty()) {
+			this.communQueue.remove(0);
+			temp = this.communQueue.get(0);
+		} else {
+			this.priorityQueue.remove(0);
+			temp = this.priorityQueue.get(0);
+		}
+		return temp;
 	}
 	
 	public String showQueues() {
-		return "";
+		int i = 0,j = 0;
+		StringBuilder sb = new StringBuilder();
+		sb.append("isoso:");
+		if(this.priorityQueue.isEmpty()) {
+			sb.append("vazio");
+		}else {
+			for(Client client: this.priorityQueue) {
+				if(i++ == this.priorityQueue.size() - 1){
+					sb.append(client.getName());
+			    }else {
+			    	sb.append(client.getName() + "-");
+			    }
+			}			
+		}
+		sb.append(";");
+		sb.append("normal:");
+		if(this.communQueue.isEmpty()) {
+			sb.append("vazio");
+		}else {
+			for(Client client: this.communQueue) {
+				if(j++ == this.communQueue.size() - 1){
+					sb.append(client.getName());
+			    }else {
+			    	sb.append(client.getName() + "-");
+			    }
+			}			
+		}
+		return sb.toString();
 	}
 
-	public ArrayList<Client> getQueue() {
-		return queue;
-	}
-
-	public void setQueue(ArrayList<Client> queue) {
-		this.queue = queue;
-	}
-
-	
-	
 }
