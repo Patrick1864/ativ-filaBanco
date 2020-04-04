040ef386096c0fe85fbd7270a7a66f852d4ae1a5
Claudio Henrique <>
2020-04-04 17:51:12 -0300

    Update App.java
    Interacao com usuario pelo console

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 
 public class App {
+	public static ManageAttendance manage = new ManageAttendance(10);
+	public static Scanner scanner = new Scanner(System.in); 
 
 	public static void main(String[] args) {
 		
@@ -16,6 +18,68 @@ public class App {
 
 		System.out.println(manage.isEmpty());
 		System.out.println(manage.showQueues()); */
+
+		int op = 0;
+		do {
+				System.out.println("******************Bem-vindo*****************");
+				System.out.println("Opções:");
+				System.out.println("1- Chegada do cliente na agência");
+				System.out.println("2- Verificar quem é o próximo a ser atendido");
+				System.out.println("3- Atender um cliente");
+				System.out.println("4- Exibir as filas");
+				System.out.println("5- Finalizar o programa\n");
+				try{
+						op = scanner.nextInt();
+				}catch(Exception e){
+						System.out.println("Apenas números por favor.");
+				}
+				scanner.nextLine();
+						switch (op) {
+								case 1:
+										System.out.println("Chegou um cliente!!");
+										newClient();
+										break;
+								case 2:
+												System.out.println("Proximo cliente:\n");
+												if(manage.showNext() != null){
+														System.out.println(manage.showNext());
+												}else{
+														System.out.println("Não há clientes.");
+												}
+										break;
+								case 3:
+												if(!manage.isEmpty()){
+														if(manage.showNext() != null){
+																System.out.println("O próximo cliente está indo até você");
+																System.out.println(manage.getNext());
+																System.out.println("Cliente atendido. PRÓXIMO!");
+														}else{
+																System.out.println("Não há ninguém na fila.");
+														}
+												}else {
+														System.out.println("Não há ninguém na fila.");
+												}
+										break;
+								case 4:
+									 if(manage.showQueues() != null){
+											System.out.println(manage.showQueues());
+										}else{
+												System.out.println("Não há ninguém no momento");
+										}
+										break;
+								case 5:
+										if (!manage.isEmpty()) {
+												System.out.println("Encerramento bloqueado, tem cliente na fila!");
+										} else {
+											System.out.println("Sistema encerrado");
+												op = 6;
+										}
+										break;
+								default:
+										System.out.println("Opção inválida. Selecione outra opção.");
+										break;
+						}
+		} while (op != 6);	
 
 	}
 
